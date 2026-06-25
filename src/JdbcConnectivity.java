1af3790bad3e420314edab3ce2a9f08fe943fd76
DevGuru <guruseelan29aug2005@gmail.com>
2026-06-25 09:39:13 +0530

    Intial Commit

---
+++ src/JdbcConnectivity.java
@@ -0,0 +1,22 @@
+import java.sql.*;
+
+public class JdbcConnectivity {
+
+    public static void main(String[] args) throws SQLException {
+
+        String query = "Select name From hosteller where hid = 25";
+        String url ="jdbc:mysql://localhost:3306/hibernate_demo";
+        String userName = "root";
+        String password = "29aug2005";
+
+        Connection connection = DriverManager.getConnection(url,userName,password);
+        Statement statement = connection.createStatement();
+        ResultSet rs = statement.executeQuery(query);
+        rs.next();
+
+        String res = rs.getString(1);
+        System.out.println(res);
+
+        connection.close();
+    }
+}
