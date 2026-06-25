04d9de7d421062869d570dec0fb24d77d76e28e9
DevGuru <guruseelan29aug2005@gmail.com>
2026-06-25 09:58:49 +0530

    Query Db

--- src/JdbcConnectivity.java
+++ src/JdbcConnectivity.java
@@ -4,14 +4,15 @@ public class JdbcConnectivity {
 
     public static void main(String[] args) throws SQLException {
 
-        String query = "Select name From hosteller where hid = 25";
+        String query = "Select name From hosteller where hid = 23";
         String url ="jdbc:mysql://localhost:3306/hibernate_demo";
-        String userName = "root";
-        String password = "29aug2005";
+        String userName = "****";
+        String password = "****";
 
         Connection connection = DriverManager.getConnection(url,userName,password);
         Statement statement = connection.createStatement();
         ResultSet rs = statement.executeQuery(query);
+
         rs.next();
 
         String res = rs.getString(1);
